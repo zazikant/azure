1bed21550404ec4c92866fb1a005f03c559cfaef
Shashikant Zarekar <zazikant@gmail.com>
2023-09-23 19:55:34 +0530

    matt

--- slack/app.py
+++ slack/app.py
@@ -6,11 +6,13 @@ from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, request, abort
-# from functions import draft_email
+from functions import draft_email
 import logging
 from functools import wraps
 import time
 import sys
+
+
 
 # Configure the logging level and format
 logging.basicConfig(
@@ -19,6 +21,7 @@ logging.basicConfig(
     stream=sys.stdout,
 )
 
+
 # Load environment variables from .env file
 load_dotenv(find_dotenv())
 
@@ -26,6 +29,12 @@ load_dotenv(find_dotenv())
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
 SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+LANGCHAIN_API_KEY = os.environ["LANGCHAIN_API_KEY"]
+LANGCHAIN_ENDPOINT = os.environ["LANGCHAIN_ENDPOINT"]
+LANGCHAIN_PROJECT = os.environ["LANGCHAIN_PROJECT"]
+LANGCHAIN_TRACING_V2 = os.environ["LANGCHAIN_TRACING_V2"]
+OPENAI_API_MODEL = os.environ["OPENAI_API_MODEL"]
 
 # Initialize the Slack app
 app = App(token=SLACK_BOT_TOKEN)
@@ -36,32 +45,32 @@ flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
 
 
-def require_slack_verification(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if not verify_slack_request():
-            abort(403)
-        return f(*args, **kwargs)
+# def require_slack_verification(f):
+#     @wraps(f)
+#     def decorated_function(*args, **kwargs):
+#         if not verify_slack_request():
+#             abort(403)
+#         return f(*args, **kwargs)
 
-    return decorated_function
+#     return decorated_function
 
 
-def verify_slack_request():
-    # Get the request headers
-    timestamp = request.headers.get("X-Slack-Request-Timestamp", "")
-    signature = request.headers.get("X-Slack-Signature", "")
+# def verify_slack_request():
+#     # Get the request headers
+#     timestamp = request.headers.get("X-Slack-Request-Timestamp", "")
+#     signature = request.headers.get("X-Slack-Signature", "")
 
-    # Check if the timestamp is within five minutes of the current time
-    current_timestamp = int(time.time())
-    if abs(current_timestamp - int(timestamp)) > 60 * 5:
-        return False
+#     # Check if the timestamp is within five minutes of the current time
+#     current_timestamp = int(time.time())
+#     if abs(current_timestamp - int(timestamp)) > 60 * 5:
+#         return False
 
-    # Verify the request signature
-    return signature_verifier.is_valid(
-        body=request.get_data().decode("utf-8"),
-        timestamp=timestamp,
-        signature=signature,
-    )
+#     # Verify the request signature
+#     return signature_verifier.is_valid(
+#         body=request.get_data().decode("utf-8"),
+#         timestamp=timestamp,
+#         signature=signature,
+#     )
 
 
 def get_bot_user_id():
@@ -94,6 +103,9 @@ def my_function(text):
     return response
 
 
+client = WebClient(token=SLACK_BOT_TOKEN)
+
+
 @app.event("app_mention")
 def handle_mentions(body, say):
     """
@@ -108,18 +120,26 @@ def handle_mentions(body, say):
 
     mention = f"<@{SLACK_BOT_USER_ID}>"
     text = text.replace(mention, "").strip()
-    logging.info("Received text: " + text.replace("\n", " "))
 
     say("Sure, I'll get right on that!")
     # response = my_function(text)
-    # response = draft_email(text)
-    # logging.info("Generated response: " + response.replace("\n", " "))
-    # say(response)
+    response = draft_email(text)
+
+    # Upload the PNG file as an attachment
+    try:
+        response = client.files_upload(
+            channels=body["event"]["channel"],
+            file="/exports/charts/temp_chart.png",
+            title="Here is the chart you requested.",
+        )
+        print(response)
+    except SlackApiError as e:
+        print(f"Error uploading file: {e.response['error']}")
 
 
 # Demo
 @flask_app.route("/slack/events", methods=["POST"])
-@require_slack_verification
+# @require_slack_verification
 def slack_events():
     """
     Route for handling Slack events.
--- slack/functions.py
+++ slack/functions.py
@@ -27,10 +27,6 @@ import io
 load_dotenv(find_dotenv())
 
 load_dotenv()
-API_KEY = os.environ.get("API_KEY")
-HUGGINGFACEHUB_API_TOKEN = os.environ["HUGGINGFACEHUB_API_TOKEN"]
-openai.api_key = os.environ["OPENAI_API_KEY"]
-
 
 from dotenv import find_dotenv, load_dotenv
 from langchain import OpenAI
@@ -44,58 +40,56 @@ from pandasai import SmartDataframe
 from pandasai.llm import OpenAI
 
     
-# def draft_email(user_input):
-# Define the API endpoint URL and parameters
-url = "http://13.232.224.37:8080/aurum/rest/v1/location/db/findall"
-params = {
-    "project_id": 1,
-    "user_id": 640,
-    "token": "7efbfacb7556e57d0702",
-    "page_size": 2
-}
+def draft_email(user_input):
+    # Define the API endpoint URL and parameters
+    url = "http://13.232.224.37:8080/aurum/rest/v1/location/db/findall"
+    params = {
+        "project_id": 1,
+        "user_id": 640,
+        "token": "7efbfacb7556e57d0702",
+        "page_size": 5
+    }
 
-#
+    #
 
-llm = OpenAI()
+    llm = OpenAI()
 
-# # Make a GET request for each page and extract the desired fields
-locations = []
-for page_num in range(1, 4):
-    params["page_num"] = page_num
-    response = requests.get(url, params=params)
-    data = response.json()
-    for record in data["records"]:
-        location = {
-            "location_id": record["location_id"],
-            "location_name": record["location_name"]
-        }
-        locations.append(location)
-
-
-# # Write the locations to a CSV file
-with open("/shashi/locations.csv", "w", newline="") as csvfile:
-    fieldnames = ["location_id", "location_name"]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for location in locations:
-        writer.writerow(location)
-
-# repo_id = "tiiuae/falcon-7b-instruct"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
-# llm = HuggingFaceHub(
-#     repo_id=repo_id, model_kwargs={"temperature": 0.1, "max_new_tokens": 500}
-# )
-
-df = pd.read_csv("/shashi/locations.csv")
-
-sdf = SmartDataframe(df, config={"llm": llm})
-
-sdf.chat("what are top 5 location_names with highest occurence")
-
-# code = print(sdf.last_code_generated).__str__()
+    # # Make a GET request for each page and extract the desired fields
+    locations = []
+    for page_num in range(1, 4):
+        params["page_num"] = page_num
+        response = requests.get(url, params=params)
+        data = response.json()
+        for record in data["records"]:
+            location = {
+                "location_id": record["location_id"],
+                "location_name": record["location_name"]
+            }
+            locations.append(location)
 
 
+    # # Write the locations to a CSV file
+    with open("/shashi/locations.csv", "w", newline="") as csvfile:
+        fieldnames = ["location_id", "location_name"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for location in locations:
+            writer.writerow(location)
 
-    # return response
+    # repo_id = "tiiuae/falcon-7b-instruct"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
+    # llm = HuggingFaceHub(
+    #     repo_id=repo_id, model_kwargs={"temperature": 0.1, "max_new_tokens": 500}
+    # )
+
+    df = pd.read_csv("/shashi/locations.csv")
+
+    sdf = SmartDataframe(df, config={"llm": llm})
+
+    sdf.chat(user_input)
+
+    response = print(sdf.last_code_generated).__str__()
+
+    return response
 
 # agent = create_pandas_dataframe_agent(llm, df, verbose=True)
 
