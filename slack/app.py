401f12addae91e8049304226813fe04c342a053d
Shashikant Zarekar <zazikant@gmail.com>
2023-09-23 14:53:56 +0530

    master

--- slack/app.py
+++ slack/app.py
@@ -156,7 +156,6 @@ def slack_events():
 # Run the Flask app
 if __name__ == "__main__":
     logging.info("Flask app started")
-    flask_app.run(host="0.0.0.0", port=8000)
     
     
 #when you want to work with pandas, do that. pandas agent doesnt allow you to save files.
