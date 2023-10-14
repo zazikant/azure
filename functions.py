5efe9b956fd48892c5eccc33a284b9b53e5bae81
Shashikant Zarekar <zazikant@gmail.com>
2023-10-14 21:41:55 +0530

    sd

--- functions.py
+++ functions.py
@@ -1,80 +1,122 @@
-import os
-import openai
-import pprint
-import json
-import requests
-import csv
-import io
-import matplotlib.pyplot as plt
-from IPython.core.display import HTML
-import pandas as pd
 from dotenv import find_dotenv, load_dotenv
-
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
 from langchain.prompts.chat import (
     ChatPromptTemplate,
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
-from pandasai import PandasAI
+import os
+import openai
+import pprint
+import json
+import pandas as pd
 from pandasai.llm.openai import OpenAI
-from langchain import HuggingFaceHub
-from langchain.document_loaders import PyPDFLoader
+from dotenv import load_dotenv
+import re
 
-load_dotenv(find_dotenv())
+import requests
+import csv
 
-load_dotenv()
-
-
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-LANGCHAIN_API_KEY = os.environ["LANGCHAIN_API_KEY"]
-LANGCHAIN_ENDPOINT = os.environ["LANGCHAIN_ENDPOINT"]
-LANGCHAIN_PROJECT = os.environ["LANGCHAIN_PROJECT"]
-LANGCHAIN_TRACING_V2 = os.environ["LANGCHAIN_TRACING_V2"]
-OPENAI_API_MODEL = os.environ["OPENAI_API_MODEL"]
+import matplotlib.pyplot as plt
+import io
 
 from dotenv import find_dotenv, load_dotenv
-from langchain import OpenAI
-from langchain.chat_models import ChatOpenAI
 
 import pandas as pd
-
-
 from pandasai import SmartDataframe
 from pandasai.llm import OpenAI
+
+from langchain.document_loaders import PyPDFLoader
+
+from dotenv import load_dotenv
+
+import os
+import openai
+from langchain.llms import OpenAI
+from dotenv import load_dotenv, find_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.output_parsers import ResponseSchema
+from langchain.output_parsers import StructuredOutputParser
+from langchain.memory import ConversationSummaryBufferMemory
+
+from langchain.chains.summarize import load_summarize_chain
+
+from langchain.document_loaders import DirectoryLoader, CSVLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+from langchain.prompts import ChatPromptTemplate
+from langchain import PromptTemplate, LLMChain
+
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
+SLACK_BOT_USER_ID = os.environ["SLACK_BOT_USER_ID"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
  
 
-def draft_email(user_input):
-    
-    from langchain.document_loaders import DirectoryLoader, CSVLoader
+def parser(text):
+ 
+    llm = OpenAI()
+
+    context = text.strip()
+
+    email_schema = ResponseSchema(
+        name="email_parser",
+        description="extract the email id from the text. If required, strip and correct it in format like sample@xyz.com. Only provide these words. If no email id is present, return null@null.com",
+    )
+    subject_schema = ResponseSchema(
+        name="content", description="Just extract the content removing email ids. Do not add any interpretation."
+    )
+
+    response_schemas = [email_schema, subject_schema]
+
+    parser = StructuredOutputParser.from_response_schemas(response_schemas)
+    format_instructions = parser.get_format_instructions()
+
+    template = """
+    Interprete the text and evaluate the text.
+    email_parser: extract the email id from the text. Only provide these words. If no email id is present, return null@null.com. Use 1 line.
+    content: Just extract the content removing email ids. Do not add any interpretation.
+
+    text: {context}
+
+    Just return the JSON, do not add ANYTHING, NO INTERPRETATION!
+    {format_instructions}:"""
+
+    #imprtant to have the format instructions in the template represented as {format_instructions}:"""
+
+    #very important to note that the format instructions is the json format that consists of the output key and value pair. It could be multiple key value pairs. All the context with input variables should be written above that in the template.
+
+    prompt  = PromptTemplate(
+        input_variables=["context", "format_instructions"],
+        template=template
+    )
+
+    chain = LLMChain(llm=llm, prompt=prompt, output_key= "testi")
+    response = chain.run({"context": context, "format_instructions": format_instructions})
+
+    output_dict = parser.parse(response)
+    return output_dict
+
+def draft_email(user_input):    
 
     loader = DirectoryLoader(
         "./shashi", glob="**/*.csv", loader_cls=CSVLoader, show_progress=True
     )
-    docs = loader.load()    
-    
-    
+    docs = loader.load()
+
     #textsplitter-----------------
 
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=450,
+        chunk_size=400,
         chunk_overlap=2,
     )
 
     docs = text_splitter.split_documents(docs)
     # print(docs[3].page_content)
-    #-----------------    
-    
+    #-----------------
+
     from langchain.embeddings import OpenAIEmbeddings
     openai_embeddings = OpenAIEmbeddings()
-
-    # from langchain.embeddings import HuggingFaceEmbeddings
-    # openai_embeddings = HuggingFaceEmbeddings(model_name="all-mpnet-base-v2")
-
-    #loading vectors into vector db-----------------
 
     from langchain.vectorstores.faiss import FAISS
     import pickle
@@ -82,8 +124,7 @@ def draft_email(user_input):
     #Very important - db below is used for similarity search and not been used by agents in tools
 
     db = FAISS.from_documents(docs, openai_embeddings)
-
-
+    
     import pickle
 
     with open("db.pkl", "wb") as f:
@@ -91,27 +132,14 @@ def draft_email(user_input):
         
     with open("db.pkl", "rb") as f:
         db = pickle.load(f)
-        
-    query = user_input
-    docs = db.similarity_search(query, k=8)
-
-    # HTML(f'<div style="width:50%">{docs[0].page_content}</div>')
-    # print(docs[0].page_content)
-
-
-    import os
-    import openai
-    from langchain.llms import OpenAI
-    from langchain.chains import LLMChain, SequentialChain
-    from dotenv import load_dotenv, find_dotenv
-    from langchain.prompts import ChatPromptTemplate
-    from langchain import HuggingFaceHub
-    from langchain import PromptTemplate, LLMChain
-    from langchain.chat_models import ChatOpenAI
-    from langchain.output_parsers import ResponseSchema
-    from langchain.output_parsers import StructuredOutputParser
-    from langchain.memory import ConversationSummaryBufferMemory
-
+    
+    parser_output = parser(user_input)
+    
+    email = parser_output["email_parser"]
+    
+    content = parser_output["content"]
+    
+    docs = db.similarity_search(content, k=8)
 
     llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7)
 
@@ -120,34 +148,29 @@ def draft_email(user_input):
     # you are a pediatric dentist and you are writing a key features serial wise for following information: 
 
     # text: {context}
-    # """
-
-    #blog post
-    template = """
-    You are a SEO expert having expertise in creating landing pages. The landing page should should effectively capture the key points, insights, and information from the Context.
-
-    Focus on maintaining a coherent flow and using proper grammar and language.
-
-    Incorporate relevant headings, subheadings, and bullet points to organize the content.
-
-    Ensure that the tone of the blog post is engaging and informative, catering to {target_audience} audience.
-
-    Feel free to enhance the transcript by adding additional context, examples, and explanations where necessary.
-
-    The goal is to convert context into a polished and valuable written resource while maintaining accuracy and coherence.
-
-    text: {context}
-
+    # """    
+    map_prompt = """
+    Write a concise summary of the following:
+    "{text}"
+    CONCISE SUMMARY:
     """
-    target_audience = """Age group of 35-45 years old people, who are looking for BIM services"""
+    
+    map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text"])
+    
+    combine_prompt = """
+    You are a summarisation expert. Focus on maintaining a coherent flow and using proper grammar and language. Write a detailed summary of the following text:
+    "{text}"
+    SUMMARY:
+    """
+    
+    combine_prompt_template = PromptTemplate(template=combine_prompt, input_variables=["text"])
+    
+    summary_chain = load_summarize_chain(llm=llm,
+                                     chain_type='map_reduce',
+                                     map_prompt=map_prompt_template,
+                                     combine_prompt=combine_prompt_template, verbose=True
+                                    )
 
-    prompt  = PromptTemplate(
-        input_variables=["context", "target_audience"],
-        template=template
-    )
+    response = summary_chain.run({"input_documents": docs})
 
-    chain = LLMChain(llm=llm, prompt=prompt, output_key= "testi")
-    response = chain.run({"context": docs, "target_audience": target_audience})
-
-    return response
-#G
+    return email, response
