752b8a289d358a0e2b1b14aba9051f45fd0c5977
Shashikant Zarekar <zazikant@gmail.com>
2023-10-15 11:15:06 +0530

    f

--- app.py
+++ app.py
@@ -15,9 +15,6 @@ import json
 import requests
 
 
-# Load environment variables from .env file
-load_dotenv(find_dotenv())
-
 # Set Slack API credentials
 SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
 SLACK_SIGNING_SECRET = os.environ["SLACK_SIGNING_SECRET"]
@@ -37,34 +34,6 @@ signature_verifier = SignatureVerifier(SLACK_SIGNING_SECRET)
 # Initialize the Flask app
 flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
-
-def require_slack_verification(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if not verify_slack_request():
-            abort(403)
-        return f(*args, **kwargs)
-
-    return decorated_function
-
-
-def verify_slack_request():
-    # Get the request headers
-    timestamp = request.headers.get("X-Slack-Request-Timestamp", "")
-    signature = request.headers.get("X-Slack-Signature", "")
-
-    # Check if the timestamp is within five minutes of the current time
-    current_timestamp = int(time.time())
-    if abs(current_timestamp - int(timestamp)) > 60 * 5:
-        return False
-
-    # Verify the request signature
-    return signature_verifier.is_valid(
-        body=request.get_data().decode("utf-8"),
-        timestamp=timestamp,
-        signature=signature,
-    )
-
 
 def get_bot_user_id():
     """
@@ -136,7 +105,6 @@ def handle_mentions(body, say):
 
 # Demo
 @flask_app.route("/slack/events", methods=["POST"])
-@require_slack_verification
 def slack_events():
     """
     Route for handling Slack events.
